ce087294550f4616077c0ad506e357693d441ec6
Bhekinkosi <ndumiso5154kk@gmail.com>
2022-04-11 19:42:19 -0700

    Package

---
+++ Models/Media.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Sibahlenkosini.Models
+{
+    public class Media
+    {
+        public string Id { get; set; } 
+        public string Link { get; set; }
+        public string LinkType { get; set; } 
+        public DateTime DateCreated { get; set; } 
+
+    }
+}
